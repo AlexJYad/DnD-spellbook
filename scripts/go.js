8d361759de0e0116877912a3fbd5c54752c305ac
AlexJYad <alexanderjacovlev@gmail.com>
2024-06-08 22:24:56 +0200

    debug log/pass

--- scripts/go.js
+++ scripts/go.js
@@ -11,15 +11,14 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     });
   
     // Определяем URL для перенаправления на основе данных формы
-    console.log(data);
-    if (data.login === 'admin') {
-        window.location.href = 'HTML/cards/cardsDM.html';
-    } else if (data.login === 'Talico') {
-        window.location.href = './HTML/cards/cardsTaliko.html';
-    } else if (data.login === 'Olay') {
-        window.location.href = './HTML/cards/cardsOlay.html';
-    } else if (data.login === 'Eugen') {
-        window.location.href = './HTML/cards/cardsEugen.html';
+    if (data.login === 'admin' & data.pwd === 'admin') {
+        window.location.href = '/HTML/cards/cardsDM.html';
+    } else if (data.login === 'Talico'  & data.pwd === 'C2aGY') {
+        window.location.href = '/HTML/cards/cardsTaliko.html';
+    } else if (data.login === 'Olay'  & data.pwd === 'Dy0tN') {
+        window.location.href = '/HTML/cards/cardsOlay.html';
+    } else if (data.login === 'Eugen'  & data.pwd === 'HC5sp') {
+        window.location.href = '/HTML/cards/cardsEugen.html';
     } else {
         alert('Wrond log/pass')
     }
