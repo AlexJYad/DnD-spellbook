37f1445d9507c48d26f6a95c556881a710d4ad23
AlexJYad <alexanderjacovlev@gmail.com>
2024-10-09 18:27:02 +0200

    marquee fix v2

--- src/components/Marquee/Marquee.js
+++ src/components/Marquee/Marquee.js
@@ -1,4 +1,4 @@
-// import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ReactMarquee from "react-fast-marquee";
 import "./Marquee.css";
 
