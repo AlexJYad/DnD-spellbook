b3bf4c132fabf6cc453064e72a051a2c855099e0
AlexJYad <alexanderjacovlev@gmail.com>
2024-10-11 20:10:30 +0200

    --fix 'access rights'

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,7 @@ import "../CSS/HomePage.css";
 import news from "../data/news.html";
 
 export default function HomePage() {
+   const token = localStorage.getItem("token");
    return (
       <div className="inner-main">
          <h1>LOGIN</h1>
@@ -17,46 +18,50 @@ export default function HomePage() {
                   Правила
                </a>
             </li>
-            <li>
-               <a
-                  href=" https://drive.google.com/drive/folders/1CNGQq8Rxdielqe_IJ5XP_kt_q3XPxNDb"
-                  data-symbol="📂"
-                  target="_blank"
-                  rel="noreferrer"
-               >
-                  Файлы игры
-               </a>
-            </li>
-            <li>
-               <a
-                  href="https://w2g.tv/?r=16dtz6alo4ag9jdxby"
-                  data-symbol="🎶"
-                  target="_blank"
-                  rel="noreferrer"
-               >
-                  Атмосферная музыка для игры
-               </a>
-            </li>
-            <li>
-               <a
-                  href="https://rolldicewithfriends.com/rooms/blackandwight"
-                  data-symbol="🎲"
-                  target="_blank"
-                  rel="noreferrer"
-               >
-                  Dice roll
-               </a>
-            </li>
-            <li>
-               <a
-                  href="https://docs.google.com/spreadsheets/d/14M-Oa3vs3zgYOY1gd_GiPmWjKFX5l1TY3aZdPUdcfDM/edit?usp=drive_link"
-                  data-symbol="📅"
-                  target="_blank"
-                  rel="noreferrer"
-               >
-                  Dating App
-               </a>
-            </li>
+            {token !== "test" && (
+               <>
+                  <li>
+                     <a
+                        href=" https://drive.google.com/drive/folders/1CNGQq8Rxdielqe_IJ5XP_kt_q3XPxNDb"
+                        data-symbol="📂"
+                        target="_blank"
+                        rel="noreferrer"
+                     >
+                        Файлы игры
+                     </a>
+                  </li>
+                  <li>
+                     <a
+                        href="https://w2g.tv/?r=16dtz6alo4ag9jdxby"
+                        data-symbol="🎶"
+                        target="_blank"
+                        rel="noreferrer"
+                     >
+                        Атмосферная музыка для игры
+                     </a>
+                  </li>
+                  <li>
+                     <a
+                        href="https://rolldicewithfriends.com/rooms/blackandwight"
+                        data-symbol="🎲"
+                        target="_blank"
+                        rel="noreferrer"
+                     >
+                        Dice roll
+                     </a>
+                  </li>
+                  <li>
+                     <a
+                        href="https://docs.google.com/spreadsheets/d/14M-Oa3vs3zgYOY1gd_GiPmWjKFX5l1TY3aZdPUdcfDM/edit?usp=drive_link"
+                        data-symbol="📅"
+                        target="_blank"
+                        rel="noreferrer"
+                     >
+                        Dating App
+                     </a>
+                  </li>
+               </>
+            )}
          </ul>
          <div
             className="news-container"
