157b314b9eabd260115e2548df8f5534414d34bc
AlexJYad <alexanderjacovlev@gmail.com>
2024-12-29 15:45:45 +0100

    fix img

--- src/pages/Maps/MapsWorldPage.js
+++ src/pages/Maps/MapsWorldPage.js
@@ -3,7 +3,7 @@ import React from "react";
 import img from "../../img/maps/world.png";
 
 const MapsWorldPage = () => {
-   return <img src={img} width="100%" />;
+   return <img src={img} width="100%" alt="Карта Мира" />;
 };
 
 export default MapsWorldPage;
