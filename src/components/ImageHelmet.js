aaa08e7e7ac7dfc1a408279d169ff786e5c87c7e
AlexJYad <alexanderjacovlev@gmail.com>
2025-01-21 13:30:20 +0100

    fix img

--- src/components/ImageHelmet.js
+++ src/components/ImageHelmet.js
@@ -4,88 +4,48 @@ import { Helmet } from "react-helmet";
 const ImageHelmet = () => {
    return (
       <Helmet>
-         <link
-            rel="preload"
-            as="image"
-            href="../../img/bookmarks/cristal.webp"
-         />
-         <link rel="preload" as="image" href="../../img/bookmarks/hand.webp" />
-         <link rel="preload" as="image" href="../../img/bookmarks/mouth.webp" />
+         <link rel="lazy" as="image" href="../../img/bookmarks/cristal.webp" />
+         <link rel="lazy" as="image" href="../../img/bookmarks/hand.webp" />
+         <link rel="lazy" as="image" href="../../img/bookmarks/mouth.webp" />
 
          <link
-            rel="preload"
+            rel="lazy"
             as="image"
             href="../../img/background/name-frame.webp"
          />
+         <link rel="lazy" as="image" href="../../img/background/flag--S.webp" />
          <link
-            rel="preload"
-            as="image"
-            href="../../img/background/flag--S.webp"
-         />
-         <link
-            rel="preload"
+            rel="lazy"
             as="image"
             href="../../img/background/flag--XS.webp"
          />
+         <link rel="lazy" as="image" href="../../img/background/flag--M.webp" />
          <link
-            rel="preload"
-            as="image"
-            href="../../img/background/flag--M.webp"
-         />
-         <link
-            rel="preload"
+            rel="lazy"
             as="image"
             href="../../img/background/flag--M__2.webp"
          />
+         <link rel="lazy" as="image" href="../../img/background/crown.webp" />
          <link
-            rel="preload"
-            as="image"
-            href="../../img/background/crown.webp"
-         />
-         <link
-            rel="preload"
+            rel="lazy"
             as="image"
             href="../../img/background/crystal-ball.webp"
          />
-         <link rel="preload" as="image" href="../../img/background/dice.webp" />
-         <link rel="preload" as="image" href="../../img/background/eye.webp" />
+         <link rel="lazy" as="image" href="../../img/background/dice.webp" />
+         <link rel="lazy" as="image" href="../../img/background/eye.webp" />
          <link
-            rel="preload"
+            rel="lazy"
             as="image"
             href="../../img/background/hits-temporary.webp"
          />
-         <link
-            rel="preload"
-            as="image"
-            href="../../img/background/hits-d.webp"
-         />
-         <link rel="preload" as="image" href="../../img/background/slot.webp" />
-         <link rel="preload" as="image" href="../../img/background/HP.webp" />
-         <link
-            rel="preload"
-            as="image"
-            href="../../img/background/sabaton.webp"
-         />
-         <link
-            rel="preload"
-            as="image"
-            href="../../img/background/flag--L.png"
-         />
-         <link
-            rel="preload"
-            as="image"
-            href="../../img/background/scull.webp"
-         />
-         <link
-            rel="preload"
-            as="image"
-            href="../../img/background/heart.webp"
-         />
-         <link
-            rel="preload"
-            as="image"
-            href="../../img/background/scroll.webp"
-         />
+         <link rel="lazy" as="image" href="../../img/background/hits-d.webp" />
+         <link rel="lazy" as="image" href="../../img/background/slot.webp" />
+         <link rel="lazy" as="image" href="../../img/background/HP.webp" />
+         <link rel="lazy" as="image" href="../../img/background/sabaton.webp" />
+         <link rel="lazy" as="image" href="../../img/background/flag--L.png" />
+         <link rel="lazy" as="image" href="../../img/background/scull.webp" />
+         <link rel="lazy" as="image" href="../../img/background/heart.webp" />
+         <link rel="lazy" as="image" href="../../img/background/scroll.webp" />
       </Helmet>
    );
 };
